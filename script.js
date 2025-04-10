b6a8f9ab9ecba92b37b59156e1c52e07e45ea3af
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-10 15:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+let form = document.querySelector('form')
+let inputBox = document.getElementById('name')
+let inputNumber = document.getElementById('year');
+let h3 = document.getElementById('url');
+
+form.addEventListener("submit" , (e)=>{
+	let name = inputBox.value;
+	let year = inputNumber.value;
+	h3.innerText = `https://localhost:8080/${name}`;
+})
