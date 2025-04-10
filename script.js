cf263188840581e3bcff624248c3c33d7ca5fbe7
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-10 16:02:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ form.addEventListener("submit" , (e)=>{
 	e.preventDefault();
 	let name = inputBox.value;
 	let year = inputNumber.value;
-	if(!name && !year){
-		h3.innerText = `https://localhost:8080/`;
-	}else if(name && year){
+	if(name && year){
 		h3.innerText = `https://localhost:8080/?name=${name}&year=${year}`;
-	}else if(name.length == 0 && year){
+		
+	}else if(name && !year){
+		h3.innerText = `https://localhost:8080/?name=${name}`;
+		
+	}else if(!name  && year){
 		h3.innerText = `https://localhost:8080/?year=${year}`;
 	}else{
-		h3.innerText = `https://localhost:8080/?name=${name}`;
+		h3.innerText = `https://localhost:8080/`;
 	}
-	h3.innerText = `https://localhost:8080/${name}`;
+	
 })
