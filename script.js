3e371604471471a59496d5fef3731a8014d027f4
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-10 15:45:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let inputNumber = document.getElementById('year');
 let h3 = document.getElementById('url');
 
 form.addEventListener("submit" , (e)=>{
+	e.preventDefault();
 	let name = inputBox.value;
 	let year = inputNumber.value;
 	h3.innerText = `https://localhost:8080/${name}`;
